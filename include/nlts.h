a70b1a737ec755cd126887a12031befe81022712
Emily Jakobs <emilycjakobs@gmail.com>
2021-08-04 11:28:44 -0700

    add nlts.h

---
+++ include/nlts.h
@@ -0,0 +1,12 @@
+#ifndef NLTS_NLTS_H
+#define NLTS_NLTS_H
+#include <nlts/diff.h>
+#include <nlts/embedding.h>
+#include <nlts/io.h>
+#include <nlts/mutual.h>
+#include <nlts/plot.h>
+#include <nlts/sample.h>
+#include <nlts/sys.h>
+#include <nlts/fnn.h>
+#include <nlts/std/dimension.h>
+#endif /* NLTS_NLTS_H */
